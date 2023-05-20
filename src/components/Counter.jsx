b73ef74dcca1c85fadcfde05968187c47f8879c5
shanuNP <shanunp2016@gmail.com>
2023-05-20 16:18:54 +0530

    -- conditionali rendering

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,35 +1,29 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'; 
 
-export default class Counter extends Component {
+class Counter extends Component {
 
     state = {
         count : 0,
         tags : ['tag1','tag2','tag3']
     }
 
-    counterFormater(){
-        const {count} = this.state
-        return count === 0 ? 'zero' : count;
-    }
 
-    getClasses(){
-        const {count} = this.state
-        let classes = "badge m-2 badge-"
-        classes += count === 'zero' ? "warning" : "primary"
-        return classes;
+
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>There is no tags!</p>
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>    
     }
 
 
 render() {
     return (
       <div>
-
-        <span className={this.getClasses()}>{this.counterFormater()}</span>
-        <button className="btn btn-secondary">increment</button>
-        <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-
-      </div>
+        {this.state.tags.length === 0 && "please add some tags"}
+        {this.renderTags()}
+        </div>
     )
   }
 }
+
+export default Counter;
